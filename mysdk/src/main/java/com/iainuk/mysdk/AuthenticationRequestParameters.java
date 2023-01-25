2829a17ba67750500f27f2fc8445549f2b705a55
iainuk86 <iainuk86@gmail.com>
2023-01-25 23:06:26 +1100

    Change records back to data classes

--- mysdk/src/main/java/com/iainuk/mysdk/AuthenticationRequestParameters.java
+++ mysdk/src/main/java/com/iainuk/mysdk/AuthenticationRequestParameters.java
@@ -1,5 +1,45 @@
 package com.iainuk.mysdk;
 
-public record AuthenticationRequestParameters (String sdkAppID, String sdkReferenceNumber,
-                                               String sdkTransactionID, String sdkEphemeralPublicKey,
-                                               String deviceData, String messageVersion) {}
+public class AuthenticationRequestParameters {
+    private final String sdkAppID;
+    private final String deviceData;
+    private final String messageVersion;
+    private final String sdkTransactionID;
+    private final String sdkReferenceNumber;
+    private final String sdkEphemeralPublicKey;
+
+    public AuthenticationRequestParameters(String sdkAppID, String deviceData,
+                                           String messageVersion, String sdkTransactionID,
+                                           String sdkReferenceNumber, String sdkEphemeralPublicKey) {
+        this.sdkAppID = sdkAppID;
+        this.deviceData = deviceData;
+        this.messageVersion = messageVersion;
+        this.sdkTransactionID = sdkTransactionID;
+        this.sdkReferenceNumber = sdkReferenceNumber;
+        this.sdkEphemeralPublicKey = sdkEphemeralPublicKey;
+    }
+
+    public String getSdkAppID() {
+        return sdkAppID;
+    }
+
+    public String getDeviceData() {
+        return deviceData;
+    }
+
+    public String getMessageVersion() {
+        return messageVersion;
+    }
+
+    public String getSdkTransactionID() {
+        return sdkTransactionID;
+    }
+
+    public String getSdkReferenceNumber() {
+        return sdkReferenceNumber;
+    }
+
+    public String getSdkEphemeralPublicKey() {
+        return sdkEphemeralPublicKey;
+    }
+}
--- mysdk/src/main/java/com/iainuk/mysdk/RuntimeErrorEvent.java
+++ mysdk/src/main/java/com/iainuk/mysdk/RuntimeErrorEvent.java
@@ -1,3 +1,19 @@
 package com.iainuk.mysdk;
 
-public record RuntimeErrorEvent (String errorCode, String errorMessage) {}
+public class RuntimeErrorEvent {
+    private final String errorCode;
+    private final String errorMessage;
+
+    public RuntimeErrorEvent(String errorCode, String errorMessage) {
+        this.errorCode = errorCode;
+        this.errorMessage = errorMessage;
+    }
+
+    public String getErrorCode() {
+        return errorCode;
+    }
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+}
